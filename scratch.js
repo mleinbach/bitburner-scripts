c5c94da0d08d72848af66d054e36063343139244
Mitch Leinbach <mitchjleinbach@gmail.com>
2022-12-09 22:44:57 -0500

    scratch

--- scratch.js
+++ scratch.js
@@ -1,8 +1,13 @@
+import { BatchRunner } from "./batchRunner";
 import { MockExecutionPlanBuilder } from "./executionPlan";
 
 /** @param {NS} ns */
 export async function main(ns) {
-    let a = MockExecutionPlanBuilder;
-    let executionPlan = a.build(ns, "home", 0.10);
-    ns.tprint(JSON.stringify(executionPlan))
+    let target = "n00dles";
+    let maxBatches = 2;
+    let workers = {"Mock": ["home", "home", "home", "home"]};
+    let hackAmount = 0.10;
+    let executionPlanBuilder = MockExecutionPlanBuilder;
+    let runner = new BatchRunner(ns, target, maxBatches, workers, hackAmount, executionPlanBuilder);
+    await runner.run();
 }
