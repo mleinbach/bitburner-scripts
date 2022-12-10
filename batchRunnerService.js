f04a23153c6fcc2ddc042711ba09dc0fba9d59c2
Mitch Leinbach <mitchjleinbach@gmail.com>
2022-12-10 10:50:11 -0500

    dump errors to log file like a normal person

--- batchRunnerService.js
+++ batchRunnerService.js
@@ -1,12 +1,20 @@
 import { BatchRunner } from "./batchRunner";
-import { HWGWExecutionPlanBuilder } from "./executionPlan";
+import { HWGWExecutionPlanBuilder, MockExecutionPlanBuilder } from "./executionPlan";
+import { Logger } from "./logger";
 
 /** @param {NS} ns */
 export async function main(ns) {
-    let id = ns.args[0]
-    let target = ns.args[1];
-    let maxBatches = ns.args[2];
-    let workers = JSON.parse(ns.args[3]);
-    let hackAmount = ns.args[4];
-    await new BatchRunner(ns, target, maxBatches, workers, hackAmount, HWGWExecutionPlanBuilder).run();
+    let logger = new Logger(ns, "batchRunnerService");
+    logger.disableNSLogs();
+    try {
+        logger.info("Batch runner started.")
+        let id = ns.args[0]
+        let target = ns.args[1];
+        let maxBatches = ns.args[2];
+        let workers = JSON.parse(ns.args[3]);
+        let hackAmount = ns.args[4];
+        await new BatchRunner(ns, target, maxBatches, workers, hackAmount, MockExecutionPlanBuilder).run();
+    } catch (e) {
+        logger.error(`Unhandled exception occurred:\n${e.stack}`)
+    }
 }
