84b9ec1fa183605a7b52ea02cd59473243b9cb38
Mitch Leinbach <mitchjleinbach@gmail.com>
2022-12-13 08:23:22 -0500

    hgw scripts write to port on completion

--- grow.js
+++ grow.js
@@ -1,6 +1,16 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    const [id, target, delay, port=null] = ns.args
-    await ns.sleep(delay);
-    await ns.grow(target);
+    try {
+        const [id, target, delay, batchId, port = null] = ns.args
+        const startTime = Date.now();
+        await ns.sleep(delay);
+        await ns.grow(target);
+        const endTime = Date.now();
+
+        if (port !== null) {
+            ns.tryWritePort(port, JSON.stringify({ id: id, batchId: batchId, startTime: startTime, endTime: endTime }));
+        }
+    } catch (e) {
+        ns.print(e.stack)
+    }
 }
--- hack.js
+++ hack.js
@@ -1,6 +1,17 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    const [id, target, delay, port=null] = ns.args
-    await ns.sleep(delay);
-    await ns.hack(target);
+    try {
+        const [id, target, delay, batchId, port = null] = ns.args
+        const startTime = Date.now();
+        await ns.sleep(delay);
+        await ns.hack(target);
+        const endTime = Date.now();
+
+        if (port !== null) {
+            ns.tryWritePort(port, JSON.stringify({ id: id, batchId: batchId, startTime: startTime, endTime: endTime }));
+        }
+    } catch (e) {
+        ns.print(e.stack);
+    }
+
 }
