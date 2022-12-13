3df8a9b0e377fa53495502c2d03ae508a7f06f58
Mitch Leinbach <mitchjleinbach@gmail.com>
2022-12-13 08:24:05 -0500

    removed Mock class and args cleanup

--- batchRunnerService.js
+++ batchRunnerService.js
@@ -1,19 +1,16 @@
 import { BatchRunner } from "./batchRunner";
-import { HWGWExecutionPlanBuilder, MockExecutionPlanBuilder } from "./executionPlan";
+import { HWGWExecutionPlanBuilder } from "./executionPlan";
 import { Logger } from "./logger";
 
 /** @param {NS} ns */
 export async function main(ns) {
+    const [id, target, maxBatches, workersStr, hackAmount] = ns.args;
     let logger = new Logger(ns, "batchRunnerService");
     logger.disableNSLogs();
     logger.info("started.")
     logger.debug(`main(): args=${ns.args}`)
     try {
-        let id = ns.args[0]
-        let target = ns.args[1];
-        let maxBatches = ns.args[2];
-        let workers = JSON.parse(ns.args[3]);
-        let hackAmount = ns.args[4];
+        let workers = JSON.parse(workersStr);
         await new BatchRunner(ns, target, maxBatches, workers, hackAmount, HWGWExecutionPlanBuilder).run();
     } catch (e) {
         logger.error(`Unhandled exception occurred:\n${e.stack}`)
